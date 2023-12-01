d56d766f3caf830bc850ea2abc33c0bf9a8a3b38
Wilker Lisboa <wilkersoares.wl@gmail.com>
2023-12-01 11:08:11 -0300

    First Commit

---
+++ Public/js/main.js
@@ -0,0 +1,15 @@
+function botao(){
+    window.open("./Public/assests/alpha.html", "_self");
+}
+function botaoVoltar(){
+    window.open(".../index.html", "_self");
+}
+function projeto1(){
+    window.open("https://github.com/wilkerlisboa/PDV-Ponto-De-Venda");
+}
+function projeto2(){
+    window.open("https://github.com/wilkerlisboa/login");
+}
+function artigo1(){
+    window.open("https://dev.to/wilkerlisboa");
+}
